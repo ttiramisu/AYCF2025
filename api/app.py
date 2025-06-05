910cb0542c5d1ad35138d39309254a5a7dadb574
ttiramisu <106864282+ttiramisu@users.noreply.github.com>
2025-06-05 21:19:18 +0800

    push

--- api/app.py
+++ api/app.py
@@ -1,31 +1,20 @@
 from flask import Flask, render_template
-
+from vercel_wsgi import handle_request
 
 app = Flask(__name__, static_folder='static', static_url_path='')
-
-###############################
-############ MAIN #############
-###############################
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-###############################
-############ ERROR ############
-###############################
-
 @app.errorhandler(404)
 def page_not_found(e):
-	return render_template("404.html"), 404
-
+    return render_template("404.html"), 404
 
 @app.errorhandler(500)
-def page_not_found(e):
-	return render_template("500.html"), 500
+def internal_server_error(e):
+    return render_template("500.html"), 500
 
-###############################
-######## RUN TIME CODE ########
-###############################
-if __name__ == "__main__":
-    app.run(debug=True)
+# Vercel uses this to invoke your Flask app
+def handler(environ, start_response):
+    return handle_request(app, environ, start_response)
