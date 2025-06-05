adb99177eb186eadcc48dd93546d07c61af588dc
ttiramisu <106864282+ttiramisu@users.noreply.github.com>
2025-06-05 21:28:34 +0800

    mod

--- api/app.py
+++ api/app.py
@@ -13,7 +13,3 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template("500.html"), 500
-
-# Vercel uses this to invoke your Flask app
-def handler(environ, start_response):
-    return handle_request(app, environ, start_response)
